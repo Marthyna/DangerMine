c8f7086a5eff592525c046ae544b45739d74e4b0
arturrossi <artur_rossi@hotmail.com>
2022-04-21 21:45:39 -0300

    wip: add camera wasd and rotation

---
+++ include/camera.h
@@ -0,0 +1,42 @@
+#ifndef _CAMERA_CLASS_H
+#define _CAMERA_CLASS_H
+
+#include "matrices.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <glad/glad.h>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GLFW/glfw3.h>
+
+class Camera
+{
+private:
+    GLuint program_id;
+
+    float g_CameraTheta;
+    float g_CameraPhi;
+
+    float r;
+    float y;
+    float z;
+    float x;
+
+public:
+    float g_CameraDistance;
+    bool firstClick = true;
+
+    glm::vec4 center_point;
+    glm::vec4 lookat_point;
+    glm::vec4 view_vector;
+    glm::vec4 up_vector;
+    glm::mat4 view;
+
+    Camera(GLuint program_id);
+    void update();
+    void listenForInputs(GLFWwindow *window, double *mouseXPos, double *mouseYPos, double *mouseXOffset, double *mouseYOffset);
+};
+
+#endif
